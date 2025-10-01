3f6ac4f083903717dbc12787763ea9b05e4c56f9
jeannetteetzold1980-sketch <jeannette.etzold1980@gmail.com>
2025-10-01 12:29:16 +0000

    sdfge

---
+++ __pycache__/config.py
@@ -0,0 +1,5 @@
+# Standard-Whisper-Konfiguration (kann in der GUI überschrieben werden)
+WHISPER_MODEL = "small"  # Siehe WHISPER_MODEL_OPTIONS für mehr
+WHISPER_ENGINE = "faster-whisper"  # "whisper" oder "faster-whisper"
+DEVICE = "cpu"  # "cpu" oder "cuda"
+COMPUTE_TYPE = "default"  # siehe https://opennmt.net/CTranslate2/quantization.html
